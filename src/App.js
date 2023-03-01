abe8a8e6048bd50c7fc4b424a6df01aad9d07a34
abhishek yadav <abhishekyadav9667@gmail.com>
2023-03-01 13:44:23 +0530

    password-generator

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import './app.css';
+import Passgenrator from './Componnets/Passgenrator';
+
+
+
+function App() {
+
+  const [Password, setPassword] = useState("")
+  const [Passlength, setPasslength] = useState(8)
+  const [Uppercase, setUppercase] = useState(false)
+  const [Lowercase, setLowercase] = useState(false)
+  const [Number, setNumber] = useState(false)
+  const [Symbols, setSymbols] = useState(false)
+  const [Copied, setCopied] = useState(false)
+
+  return (
+    <div className="App">
+    <Passgenrator Password={Password} setPassword={setPassword}
+    Passlength={Passlength} setPasslength={setPasslength}
+    Uppercase={Uppercase} setUppercase={setUppercase}
+    Lowercase={Lowercase} setLowercase={setLowercase}
+    Number={Number} setNumber={setNumber}
+    Symbols={Symbols} setSymbols={setSymbols}
+    Copied={Copied} setCopied={setCopied}/>
+
+     
+    </div>
+  );
+}
+
+export default App;
